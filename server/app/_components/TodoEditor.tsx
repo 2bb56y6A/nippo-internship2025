baa4f0a97de2926c4cc1a6488365160c53582345
ton95moy <towfiq280@gmail.com>
2025-08-26 07:05:23 +0000

    changed dialog-botton's color

--- server/app/_components/TodoEditor.tsx
+++ server/app/_components/TodoEditor.tsx
@@ -96,14 +96,14 @@ const TodoEditor = ({ editTargetTodo, onSubmit }): JSX.Element => {
                 className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-500"
                 onClick={() => { closeDialog(); onSubmit(todo); } }
                 >
-                  はい(yes)
+                  はい
                 </button>
 
                 <button 
-                className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-400"
+                className="px-4 py-2 bg-gray-100 rounded hover:bg-gray-200"
                 onClick={() => { closeDialog(); } }
                 >
-                  いいえ(no)
+                  いいえ  
                 </button></div>
             </dialog>
 
